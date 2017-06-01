7cd4e2ac586af54191c70ff9a022dfe60bd01e35
Bill Boughton <boughton.william@gmail.com>
2017-06-01 14:01:28 -0600

    getting all and returning with id

--- app.py
+++ app.py
@@ -21,19 +21,75 @@ mongo = PyMongo(app)
 
 @app.route('/getall')
 def getall():
-    return "hello world"
+    output=[]
+
+    itemBC = mongo.db.psupplyBC
+    for bc in itemBC.find():
+        output.append({
+        'brand_name':bc['brand_name'],
+        'item_name':bc['item_name'],
+        'quantity':bc['quantity'],
+        'item_id':str(bc['_id'])
+        })
+
+    itemID = mongo.db.psupplyID
+    for item in itemID.find():
+        output.append({
+        'brand_name':item['brand_name'],
+        'item_name':item['item_name'],
+        'quantity':item['quantity'],
+        'item_id':str(item['_id'])
+        })
+    return jsonify({'result': output})
 
 @app.route('/insert', methods=['GET', 'POST'])
 def insert():
-    if request.method == 'POST':
+    if (request.method == 'POST'):
+        barcode = request.form['barcode']
+        itemBC = mongo.db.psupplyBC
+        itemToIncrease = itemBC.find_one({'barcode':barcode})
+        if (itemToIncrease):
+            print("there is something in already")
+            itemToIncrease['quantity'] +=1
+            itemToIncrease['date_added'].append(datetime.datetime.now())
+            itemBC.save(itemToIncrease)
+            output = {'brand_name': itemToIncrease['brand_name'], 'item_name': itemToIncrease['item_name'], 'quantity':itemToIncrease['quantity']}
+            return jsonify({'result': output})
+        else:
+            print("this is a new item")
+            date=[]
+            source = requests.get('https://api.nutritionix.com/v1_1/item?upc=%s&appId=84f8ed7f&appKey=d476c24cdcdf18749e8ca0e5b9bce022' % barcode)
+            brandName = source.json()["brand_name"]
+            itemName = source.json()["item_name"]
+            serveSize = source.json()["nf_serving_size_qty"]
+            servePerCont = source.json()["nf_servings_per_container"]
+            units = source.json()["nf_serving_size_unit"]
+            date.append(datetime.datetime.now())
 
-        temp = request.form['temperature']
-        # temp1 = request.data['barcode']
-        # temp = json.loads(request.data['barcode'])
-        # print(temp)
-        print(request.form['temperature'])
-        return temp
+            itemBC.insert({'brand_name':brandName, 'item_name':itemName, 'size':serveSize, 'servings_per_container':servePerCont, 'units':units, 'barcode':barcode, 'quantity':1, 'date_added':date, 'date_removed':[]})
 
+            return "item added" + brandName + " " + itemName
+
+    return "this route is only a POST"
+
+
+@app.route('/insertman', methods=['GET', 'POST'])
+def insertman():
+    if (request.method=='POST'):
+        date = []
+        itemID = mongo.db.psupplyID
+
+        brand_name = request.form['brand_name']
+        item_name = request.form['item_name']
+        quantity = request.form['quantity']
+        for i in range(int(quantity)):
+            date.append(datetime.datetime.now())
+
+        itemID.insert({'brand_name':brand_name, 'item_name':item_name, 'quantity':quantity, 'date_added':date, 'date_removed':[]})
+
+        return "Item added " + brand_name + " " + item_name
+
+    return "This route is POST only"
 
 if __name__ == '__main__':
     app.debug = True
